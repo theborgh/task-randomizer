318336b1a67c369f6ed24b7be1b86cd52d770e75
theborgh <borghiborghi@gmail.com>
2022-10-03 14:23:44 +0200

    add first app template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,48 @@
 
 console.log("==== APP is running ====");
 
+var app = {
+  title: "Task Randomizer",
+  subtitle: "Put your life in the hands of chance",
+  options: ["Wash car", "clean room"]
+};
+
 var template = React.createElement(
-  "h1",
+  "div",
   null,
-  "Task Randomizer"
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "p",
+      null,
+      app.options.length > 0 ? "Here are your options:" : "No options"
+    ),
+    React.createElement(
+      "ol",
+      null,
+      app.options.forEach(function (option) {
+        console.log(option);
+        React.createElement(
+          "li",
+          null,
+          option
+        );
+      })
+    )
+  )
 );
+
 var root = document.getElementById("app");
 
 ReactDOM.render(template, root);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 console.log("==== APP is running ====");
 
-let template = <h1>Task Randomizer</h1>;
-let root = document.getElementById("app");
+const app = {
+  title: "Task Randomizer",
+  subtitle: "Put your life in the hands of chance",
+  options: ["Wash car", "clean room"],
+};
+
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+
+    <div>
+      <p>{app.options.length > 0 ? "Here are your options:" : "No options"}</p>
+      <ol>
+        {app.options.forEach((option) => {
+          console.log(option);
+          <li>{option}</li>;
+        })}
+      </ol>
+    </div>
+  </div>
+);
+
+const root = document.getElementById("app");
 
 ReactDOM.render(template, root);
