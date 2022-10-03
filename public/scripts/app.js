8adb02660cba49d438565185dce4710ad292f985
theborgh <borghiborghi@gmail.com>
2022-10-03 16:13:34 +0200

    add a remove items button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,42 +8,113 @@ var app = {
   options: ["Wash car", "clean room"]
 };
 
-var template = React.createElement(
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    render();
+  }
+};
+
+var onRemoveAll = function onRemoveAll() {
+  app.options = [];
+  render();
+};
+
+var render = function render() {
+  var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      null,
+      app.title
+    ),
+    React.createElement(
+      "p",
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "p",
+        null,
+        app.options.length > 0 ? "Here are your options:" : "No options"
+      ),
+      React.createElement(
+        "p",
+        null,
+        app.options.length
+      ),
+      React.createElement(
+        "button",
+        { onClick: onRemoveAll },
+        "Remove all"
+      ),
+      React.createElement(
+        "ol",
+        null,
+        app.options.forEach(function (option) {
+          console.log(option);
+          React.createElement(
+            "li",
+            null,
+            option
+          );
+        })
+      ),
+      React.createElement(
+        "form",
+        { onSubmit: onFormSubmit },
+        React.createElement("input", { type: "text", name: "option" }),
+        React.createElement(
+          "button",
+          null,
+          "Add option"
+        )
+      )
+    )
+  );
+  ReactDOM.render(template, root);
+};
+
+var count = 0;
+var addOne = function addOne() {
+  count++;
+  render();
+};
+var subtractOne = function subtractOne() {
+  count--;
+  render();
+};
+
+var template2 = React.createElement(
   "div",
   null,
   React.createElement(
     "h1",
     null,
-    app.title
+    "Count: ",
+    count
   ),
   React.createElement(
-    "p",
-    null,
-    app.subtitle
+    "button",
+    { onClick: addOne },
+    "+1"
   ),
   React.createElement(
-    "div",
-    null,
-    React.createElement(
-      "p",
-      null,
-      app.options.length > 0 ? "Here are your options:" : "No options"
-    ),
-    React.createElement(
-      "ol",
-      null,
-      app.options.forEach(function (option) {
-        console.log(option);
-        React.createElement(
-          "li",
-          null,
-          option
-        );
-      })
-    )
+    "button",
+    { onClick: subtractOne },
+    "-1"
   )
 );
 
 var root = document.getElementById("app");
 
-ReactDOM.render(template, root);
+render();
--- src/app.js
+++ src/app.js
@@ -6,23 +6,69 @@ const app = {
   options: ["Wash car", "clean room"],
 };
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+const onFormSubmit = (e) => {
+  e.preventDefault();
 
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+    render();
+  }
+};
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
+
+const render = () => {
+  const template = (
     <div>
-      <p>{app.options.length > 0 ? "Here are your options:" : "No options"}</p>
-      <ol>
-        {app.options.forEach((option) => {
-          console.log(option);
-          <li>{option}</li>;
-        })}
-      </ol>
+      <h1>{app.title}</h1>
+      <p>{app.subtitle}</p>
+
+      <div>
+        <p>
+          {app.options.length > 0 ? "Here are your options:" : "No options"}
+        </p>
+        <p>{app.options.length}</p>
+        <button onClick={onRemoveAll}>Remove all</button>
+        <ol>
+          {app.options.forEach((option) => {
+            console.log(option);
+            <li>{option}</li>;
+          })}
+        </ol>
+        <form onSubmit={onFormSubmit}>
+          <input type="text" name="option" />
+          <button>Add option</button>
+        </form>
+      </div>
     </div>
+  );
+  ReactDOM.render(template, root);
+};
+
+let count = 0;
+const addOne = () => {
+  count++;
+  render();
+};
+const subtractOne = () => {
+  count--;
+  render();
+};
+
+const template2 = (
+  <div>
+    <h1>Count: {count}</h1>
+    <button onClick={addOne}>+1</button>
+    <button onClick={subtractOne}>-1</button>
   </div>
 );
 
 const root = document.getElementById("app");
 
-ReactDOM.render(template, root);
+render();
